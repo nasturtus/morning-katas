28350db93cb1ca7fad8ad8ea3fb6066e5db51a6c
nasturtus <nasturtus@gmail.com>
2018-04-09 10:07:24 +0800

    Complete findDigit

--- src/findDigit.js
+++ src/findDigit.js
@@ -7,3 +7,14 @@ If num is negative, ignore its sign and treat it as a positive value.
 If nth is not positive, return -1.
 Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0.
 */
+
+function findDigit(num, nth) {
+    if (nth <= 0) return -1
+    
+    num = Math.abs(num)
+    numString = num.toString();
+    if (nth > numString.length) return 0
+    return parseInt(numString[numString.length - nth]);
+}
+
+module.exports = findDigit
--- test/findDigit.test.js
+++ test/findDigit.test.js
@@ -5,3 +5,36 @@
 // findDigit(0, 20)       returns 0
 // findDigit(65, 0)       returns -1
 // findDigit(24, -8)      returns -1
+
+const findDigit = require ('../src/findDigit.js')
+
+describe('testing findDigit', () => {
+    it('should return 5 if nth is equal to length of num', () => {
+        expect(findDigit(5673, 4)).toEqual(5);
+    })
+
+    it('should return 2 if nth is less than length of num', () => {
+        expect(findDigit(129,2)).toEqual(2);
+    });
+
+    it('should return 8 if nth <= length of num and ignore its negative sign', () => {
+        expect(findDigit(-2825, 3)).toEqual(8);
+    });
+
+    it('should return 0 if nth > length of num', () => {
+        expect(findDigit(-456, 4)).toEqual(0);
+    });
+    
+    it('should return 0 if nth > length of num', () => {
+        expect(findDigit(0, 20)).toEqual(0);
+    });
+
+    it('should return -1 if nth === 0', () => {
+        expect(findDigit(65, 0)).toEqual(-1);
+    });
+
+    it('should return -1 if nth < 0', () => {
+        expect(findDigit(24, -8)).toEqual(-1);
+    });
+
+})
